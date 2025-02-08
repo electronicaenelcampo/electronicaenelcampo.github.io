6031ecdf92478946514c8f267c223b388f00064f
electronicaenelcampo <55672110+electronicaenelcampo@users.noreply.github.com>
2025-02-08 21:40:03 +0000

    Deploying to gh-pages from @ electronicaenelcampo/electronicaenelcampo.github.io@62f65cc9b3122c2d6150ce1d24fbd54540e1083b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%65%6E%6A%61%6D%69%6E.%68%65%6E%72%69%71%75%65%7A@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/electronicaenelcampo", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://instagram.com/electronicaenelcampo", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-kaggle',
+        title: 'Kaggle',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://www.kaggle.com/benjahenriqueztamayo", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/benjamin-henriquez-tamayo-721b2714", "_blank");
         },
       },{
       id: 'light-theme',
